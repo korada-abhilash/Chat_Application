418e9071a7da2ca9b9c40ba32712b9f1de32f992
Korada Abhilash <abhilashkorada23@gmail.com>
2024-08-18 15:10:03 +0530

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Created a chat application using MERN stack.
---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+import Logout from "./Logout";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <div className="chat-header">
+        <div className="logout">
+        <Logout/>
+        </div>
+      </div>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+    .chat-header{
+       position : relative;
+
+       .logout{
+        position : absolute;
+        top : 8px;
+        right : 16px;
+        font-size : 18px;
+       }
+    }
+`;
